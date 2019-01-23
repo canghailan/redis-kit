c42f9d72c8bd1ceb429864e552fb807865a4c49d
huhao <415328529@qq.com>
2019-01-23 10:36:19 +0800

    根据redis官网算法，使用脚本优化RedisLock

--- src/main/java/cc/whohow/redis/util/RedisLock.java
+++ src/main/java/cc/whohow/redis/util/RedisLock.java
@@ -2,29 +2,25 @@ package cc.whohow.redis.util;
 
 import cc.whohow.redis.io.ByteBuffers;
 import cc.whohow.redis.lettuce.Lettuce;
+import cc.whohow.redis.script.RedisScriptCommands;
+import io.lettuce.core.ScriptOutputType;
 import io.lettuce.core.SetArgs;
 import io.lettuce.core.api.sync.RedisCommands;
 
 import java.nio.ByteBuffer;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
-import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.LockSupport;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 /**
  * Redis简易锁，需根据场景设计好参数，否则会导致异常情况（锁提前失效等）
  */
 public class RedisLock implements Lock {
-    private static final Pattern LINE = Pattern.compile("\n");
-    private static final Pattern KEY_VALUE = Pattern.compile(": ");
-
     protected final RedisCommands<ByteBuffer, ByteBuffer> redis;
+    protected final RedisScriptCommands redisScriptCommands;
     /**
      * 锁ID
      */
@@ -40,17 +36,22 @@ public class RedisLock implements Lock {
     /**
      * 锁秘钥，用于处理误解除非自己持有的锁
      */
-    protected final String ownerKey;
+    protected final String random;
 
     public RedisLock(RedisCommands<ByteBuffer, ByteBuffer> redis, String key, Duration minLockTime, Duration maxLockTime) {
+        this(redis, key, minLockTime, maxLockTime, UUID.randomUUID().toString());
+    }
+
+    public RedisLock(RedisCommands<ByteBuffer, ByteBuffer> redis, String key, Duration minLockTime, Duration maxLockTime, String random) {
         if (minLockTime.compareTo(maxLockTime) > 0) {
             throw new IllegalArgumentException();
         }
         this.redis = redis;
+        this.redisScriptCommands = new RedisScriptCommands(redis);
         this.key = ByteBuffers.fromUtf8(key);
         this.minLockTimeMillis = minLockTime.toMillis();
         this.maxLockTimeMillis = maxLockTime.toMillis();
-        this.ownerKey = newOwnerKey();
+        this.random = random;
     }
 
     /**
@@ -87,8 +88,7 @@ public class RedisLock implements Lock {
     @Override
     public boolean tryLock() {
         SetArgs setArgs = SetArgs.Builder.nx().px(maxLockTimeMillis);
-        CharSequence state = newState();
-        return Lettuce.ok(redis.set(key.duplicate(), ByteBuffers.fromUtf8(state), setArgs));
+        return Lettuce.ok(redis.set(key.duplicate(), ByteBuffers.fromUtf8(random), setArgs));
     }
 
     /**
@@ -114,27 +114,8 @@ public class RedisLock implements Lock {
      */
     @Override
     public void unlock() {
-        Map<String, String> state = getState();
-        if (state == null) {
-            throw new IllegalStateException("state == null");
-        }
-        String stateKey = state.get("key");
-        if (!ownerKey.equals(stateKey)) {
-            // key不相同表示这不是自己加的锁
-            throw new IllegalStateException(ownerKey + " <> " + stateKey);
-        }
-        long lockTime = Long.parseLong(state.get("lockTime"));
-        // 已锁定时间
-        long lockedTime = System.currentTimeMillis() - lockTime;
-        if (lockedTime < minLockTimeMillis) {
-            // 小于最小锁定时间，调整过期时间
-            long ttl = minLockTimeMillis - lockedTime;
-            redis.pexpire(key.duplicate(), ttl);
-        } else if (lockedTime < maxLockTimeMillis - minLockTimeMillis) {
-            // 小于(最大锁定时间-最小锁定时间)，删除
-            redis.del(key.duplicate());
-        }
-        // 接近最大锁定时间，等待过期
+        redisScriptCommands.eval("cad", ScriptOutputType.INTEGER,
+                new ByteBuffer[]{key.duplicate()}, ByteBuffers.fromUtf8(random));
     }
 
     @Override
@@ -143,52 +124,10 @@ public class RedisLock implements Lock {
     }
 
     /**
-     * 读取Redis保存的锁状态
-     */
-    public Map<String, String> getState() {
-        ByteBuffer buffer = redis.get(key.duplicate());
-        if (buffer == null) {
-            return null;
-        }
-        return parseState(StandardCharsets.UTF_8.decode(buffer));
-    }
-
-    /**
      * 获取重试等待时间，默认指数退避，直到最大锁定时间的一半
      */
     protected long getRetryWaitingTime(int retryTimes) {
         long time = (long) (minLockTimeMillis * Math.exp(retryTimes));
         return Math.min(time, maxLockTimeMillis / 2);
-    }
-
-    /**
-     * 生成一个新的Key
-     */
-    protected String newOwnerKey() {
-        return UUID.randomUUID().toString();
-    }
-
-    /**
-     * 生成最新锁状态，类YAML格式
-     */
-    protected CharSequence newState() {
-        return new StringBuilder()
-                .append("minLockTime: ").append(minLockTimeMillis).append('\n')
-                .append("maxLockTime: ").append(maxLockTimeMillis).append('\n')
-                .append("key: ").append(ownerKey).append('\n')
-                // 线程ID，用于实现可重入锁
-                .append("threadId: ").append(Thread.currentThread().getId()).append('\n')
-                // 锁定时间，用于处理解锁
-                .append("lockTime: ").append(System.currentTimeMillis()).append('\n');
-    }
-
-    /**
-     * 解析锁状态
-     */
-    protected Map<String, String> parseState(CharSequence state) {
-        return LINE.splitAsStream(state)
-                .filter(line -> !line.isEmpty())
-                .map(line -> KEY_VALUE.split(line, 2))
-                .collect(Collectors.toMap(kv -> kv[0], kv -> kv[1]));
     }
 }
--- src/main/resources/cas.lua
+++ src/main/resources/cas.lua
@@ -1,4 +1,4 @@
 -- CAS key expect update
-if tostring(redis.call("GET", KEYS[1])) == ARGV[1] then
-    redis.call("SET", ARGV[2])
+if redis.call("GET", KEYS[1]) == ARGV[1] then
+    return redis.call("SET", KEYS[1], ARGV[2])
 end
